de35721b5462ab8034881a8dec66bfc7e6d2426c
danielmwakanema <danielmwakanema95@gmail.com>
2018-11-14 14:45:14 +0200

    refactor(user_spec.rb): Refactor validations as test group
    - Refactored param validations and grouped them as Params Validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,46 @@
 require_relative '../rails_helper'
 
 RSpec.describe User, :type => :model do
-  it "is valid with valid attributes" do
-    user = create(:user)
-    expect(user).to be_valid
-  end
+  describe "Params Validations" do
+    it "is valid with valid attributes" do
+      user = create(:user)
+      expect(user).to be_valid
+    end
 
-  it "is invalid without firstname" do
-    user = create(:user)
-    user.firstname = nil
-    expect(user).to_not be_valid
-  end
+    it "is invalid without firstname" do
+      user = create(:user)
+      user.firstname = nil
+      expect(user).to_not be_valid
+    end
 
-  it "is invalid without lastname" do
-    user = create(:user)
-    user.lastname = nil
-    expect(user).to_not be_valid
-  end
+    it "is invalid without lastname" do
+      user = create(:user)
+      user.lastname = nil
+      expect(user).to_not be_valid
+    end
 
-  it "is invalid without email" do
-    user = create(:user)
-    user.email = nil
-    expect(user).to_not be_valid
-  end
+    it "is invalid without email" do
+      user = create(:user)
+      user.email = nil
+      expect(user).to_not be_valid
+    end
 
-  it "is invalid without password" do
-    user = create(:user)
-    user.password = nil
-    expect(user).to_not be_valid
-  end
+    it "is invalid without password" do
+      user = create(:user)
+      user.password = nil
+      expect(user).to_not be_valid
+    end
 
-  it "is invalid without role_id" do
-    user = create(:user)
-    user.role_id = nil
-    expect(user).to_not be_valid
-  end
+    it "is invalid without role_id" do
+      user = create(:user)
+      user.role_id = nil
+      expect(user).to_not be_valid
+    end
 
-  it "is invalid without department_id" do
-    user = create(:user)
-    user.department_id = nil
-    expect(user).to_not be_valid
+    it "is invalid without department_id" do
+      user = create(:user)
+      user.department_id = nil
+      expect(user).to_not be_valid
+    end
   end
 end
