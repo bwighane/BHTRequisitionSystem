80b3cee9ed9598861b565e0eec2bd7b1bcd45428
danielmwakanema <danielmwakanema95@gmail.com>
2018-11-14 14:38:09 +0200

    test(user_spec.rb): Write tests to validate empty required fields
    - Wrote test to check for rejection of empty email
    - Wrote test to check for rejection of empty password
    - Wrote test to check for rejection of empty role_id
    - Wrote test to check for rejection of empty department_id

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,34 @@ RSpec.describe User, :type => :model do
     user.firstname = nil
     expect(user).to_not be_valid
   end
+
+  it "is invalid without lastname" do
+    user = create(:user)
+    user.lastname = nil
+    expect(user).to_not be_valid
+  end
+
+  it "is invalid without email" do
+    user = create(:user)
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it "is invalid without password" do
+    user = create(:user)
+    user.password = nil
+    expect(user).to_not be_valid
+  end
+
+  it "is invalid without role_id" do
+    user = create(:user)
+    user.role_id = nil
+    expect(user).to_not be_valid
+  end
+
+  it "is invalid without department_id" do
+    user = create(:user)
+    user.department_id = nil
+    expect(user).to_not be_valid
+  end
 end
