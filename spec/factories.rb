5636e78fe3c8e80c0b9a841b9a97e6a72e913ad9
danielmwakanema <danielmwakanema95@gmail.com>
2018-11-14 14:28:13 +0200

    chore(factories.rb): Write user factory
    - Wrote a user factory to generate fake user data

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,4 +8,12 @@ FactoryBot.define do
   factory :role do
     name { Faker::Company.profession }
   end
+
+  factory :user do
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    role, department
+  end
 end
