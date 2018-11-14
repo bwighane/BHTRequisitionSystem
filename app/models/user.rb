d8f12385b2ab896f77b63bf52eee7030c4a8dbf8
James Mwantisi <jamesmwantisi@gmail.com>
2018-11-14 10:20:21 +0200

    setting up models - user, department, role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    belongs_to :department
+    belongs_to :role
 end
--- db/migrate/20181114075456_create_users.rb
+++ db/migrate/20181114075456_create_users.rb
@@ -5,8 +5,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :lastname, null:false
       t.string :email, null:false 
       t.string :password, null:false
-      t.belongs_to :role, foreign_key :true, null:false 
-      t.belongs_to :department, foreign_key :true, null:false
+      t.belongs_to :role, foreign_key: true, null:false 
+      t.belongs_to :department, foreign_key: true, null:false
 
       t.timestamps
     end
