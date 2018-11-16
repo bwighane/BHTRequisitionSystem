95cb28f923ada78b4aab712c322f9eb9ae6cd391
James Mwantisi <jamesmwantisi@gmail.com>
2018-11-16 15:55:41 +0200

    Added an action create in the {Role Controller} to create new role

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -1,8 +1,20 @@
 
 module Api 
   module V1
-      class RolesController < ApplicationController
-      end
+    class RolesController < ApplicationController
+      def create 
+        role = Role.new(role_params)
+          if role.save 
+            render json: role 
+          end
+        end
+
+      private 
+        def role_params 
+          #white list role params
+          params.permit(:name)
+        end  
+    end
   end
 end
 
