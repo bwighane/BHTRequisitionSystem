dac60f2bff19cfb0f7e45f0f1a9f527e9962a0da
James Mwantisi <jamesmwantisi@gmail.com>
2018-11-16 10:58:49 +0200

    User Profile Update in the Users Controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,22 @@
 module Api 
     module V1
         class UsersController < ApplicationController
+            def update 
+                user = User.find(params[:id])
+                if user.update_attributes(user_params)
+                   render json: {status: 'SUCCESS', message: 'User Updated', data:user}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'User Not Updated', data:user.errors}, status: :uprocessable_entity
+                    
+                end
+            end 
+
+            private  
+                def user_params 
+                    #white listing user parameters
+                    params.permit(:firstname, :lastname, :email, :department_id, :password, :password_confirmation)
+                end
+
             
         end
     end
