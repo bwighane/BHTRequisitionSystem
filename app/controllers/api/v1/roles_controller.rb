83685f42d1d3ead19d0bae3929b6b30a3ccb2525
James Mwantisi <jamesmwantisi@gmail.com>
2018-11-16 16:00:58 +0200

    Added an action update in the {Role Controller} to update an exist role

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -2,12 +2,22 @@
 module Api 
   module V1
     class RolesController < ApplicationController
+
+      #Create new role
       def create 
         role = Role.new(role_params)
           if role.save 
             render json: role 
           end
         end
+
+      #update role
+      def update 
+        role = Role.find(params[:id])
+        if role.update_attributes(role_params)
+            render json: role    
+        end
+      end
 
       private 
         def role_params 
