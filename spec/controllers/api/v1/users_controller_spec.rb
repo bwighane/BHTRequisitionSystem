10507e8b733bfdbeb033261624c5e2d5a0b01332
danielmwakanema <danielmwakanema95@gmail.com>
2018-11-16 10:29:49 +0200

    test(users_controller_spec.rb): Add user update test
    - Added test to check for status 200
    - Added test to verify update

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,7 +2,20 @@ require 'rails_helper'
 module Api 
     module V1 
         RSpec.describe UsersController, type: :controller do
-
+            before(:each) do
+                @user = create(:user)
+            end
+            context 'Updating a user' do
+                before(:each) do
+                    patch :update, params: { id: @user.id, user: @user }, format: :json
+                end
+                it { should respond_with 200 }
+                it 'should update the user\'s name' do
+                    @user.name = 'Joseph Eli Lipnip'
+                    body = JSON.parse(response.body)
+                    expect(body['name']).to eq('Joseph Eli Lipnip')
+                end
+            end
         end
     end
 end
