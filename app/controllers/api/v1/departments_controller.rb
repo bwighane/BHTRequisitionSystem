44ecdb289b4502e47bd0234f79fd13566a5e4f36
danielmwakanema <danielmwakanema95@gmail.com>
2018-11-17 03:01:49 +0200

    test(departments_controller_spec.rb): Write department CRUD tests

---
+++ app/controllers/api/v1/departments_controller.rb
@@ -0,0 +1,11 @@
+
+module Api 
+  module V1
+    class DepartmentsController < ApplicationController
+    end
+  end
+end
+
+
+  
+
---
+++ spec/controllers/api/v1/departments_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+module Api 
+    module V1 
+        RSpec.describe DepartmentsController, type: :controller do
+            before(:each) do
+                @department = create(:department)
+            end
+            it 'should be able to fetch all departments' do
+                departments = create_list(:department, 10)
+                get :index
+                should respond_with 200
+                body = JSON.parse(response.body)
+                expect(body.size).to eq(11)
+            end
+            it 'should be able to create a department' do
+                post :create, params: { name: 'Huma Resources' }
+                should respond_with 200
+                body = JSON.parse(response.body)
+                expect(body['name']).to eq('Human Resources')
+            end
+            it 'should be able to view a department' do
+                get :show, params: { id: @department.id }
+                should respond_with 200
+                body = JSON.parse(response.body)
+                expect(body['name']).to eq(@department.name)
+            end
+            it 'should be able to update a department' do
+                patch :update, params: { id: @department.id, name: 'Research & Development' }
+                body = JSON.parse(response.body)
+                expect(body['name']).to eq('Research & Development')
+            end
+            it 'should be able to delete a department' do
+                delete :destroy, params: { id: @department.id }
+                should respond_with 200
+            end
+        end
+    end
+end
