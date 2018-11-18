c9579100ba114d027ce665213706ae210e0ac67e
danielmwakanema <danielmwakanema95@gmail.com>
2018-11-18 18:55:08 +0200

    test(donor_spec.rb): Create donor validation tests

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,2 +1,4 @@
 class Donor < ApplicationRecord
+  default_scope { where("void = 0") }
+  validates_presence_of :name
 end
--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Donor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    donor = create(:donor)
+    expect(donor).to be_valid
+  end
+
+  it "is invalid without name" do
+    donor = create(:donor)
+    donor.name = nil
+    expect(donor).to_not be_valid
+  end
 end
